ede5aecc6f13dce54e3071940b6474ebce6ac8b8
Miha Dimec <miha.dimec@visionect.com>
2018-07-06 14:48:56 +0200

    comment debug messages

--- debug_messages.go
+++ debug_messages.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 )
 
 type counter uint
 
 func (c counter) get() uint {
-    return uint(c)
+	return uint(c)
 }
 
 func (c counter) increment() { // BUG Pointer receiver
-    fmt.Println("increment(): Before incrementing:", c)
-    c=+1 // BUG it was ment to be *c+=1 pt *c++
-    fmt.Println("increment(): After incrementing:", c)
+	// fmt.Println("increment(): Before incrementing:", c)
+	c = +1 // BUG it was ment to be *c+=1 pt *c++
+	// fmt.Println("increment(): After incrementing:", c)
 }
 
 func main() {
-    c := counter(10)
-    c.increment()
-    // expected c == 11
-    fmt.Println("final value:", c.get())
+	c := counter(10)
+	c.increment()
+	// expected c == 11
+	fmt.Println("final value:", c.get())
 }
